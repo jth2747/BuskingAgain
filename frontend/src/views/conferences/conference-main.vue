3d5af0869cc5c730787ea8009750c51308742468
jang930424@lab.ssffy.com <jang930424@naver.com>
2021-08-16 14:42:52 +0900

    0816 장용원 05

--- frontend/src/views/conferences/conference-main.vue
+++ frontend/src/views/conferences/conference-main.vue
@@ -44,8 +44,8 @@ export default {
 		})
 
 		const connect = function() {
-			// state.ws = new WebSocket('wss://' + 'i5d107.p.ssafy.io' + '/groupcall');
-      state.ws = new WebSocket('wss://' + location.host + '/groupcall');
+			state.ws = new WebSocket('wss://' + 'i5d107.p.ssafy.io' + '/groupcall');
+      // state.ws = new WebSocket('wss://' + location.host + '/groupcall');
 			state.ws.onopen = function(event) {
 				console.log(event)
 				console.log("Successfully connected to the echo websocket server...")
